562f0a453efb1687d54a06a15a979182ea3aab47
patrn1 <14832593+patrn1@users.noreply.github.com>
2024-07-01 07:26:12 +0300

    Create install_node.sh

---
+++ install_node.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /tmp
+
+mkdir node_install
+
+cd node_install
+
+wget https://nodejs.org/dist/v20.15.0/node-v20.15.0-linux-x64.tar.xz
+
+tar -Jxvf node-v20.15.0-linux-x64.tar.xz
+
+mv node*/bin/* /usr/local/bin/
+
+rm -rf /usr/include
+
+mv node*/include/node /usr/include
+
+rm -rf /usr/lib/node_modules
+
+mv node*/lib/node_modules /usr/lib
