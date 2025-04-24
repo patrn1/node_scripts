7acaa40e3e4b9270b742ae4fbedef2b56a121feb
patrn1 <14832593+patrn1@users.noreply.github.com>
2025-04-25 01:51:44 +0500

    mv with rewrite

--- install_node.sh
+++ install_node.sh
@@ -10,12 +10,12 @@ wget https://nodejs.org/dist/v20.15.0/node-v20.15.0-linux-x64.tar.xz
 
 tar -Jxvf node-v20.15.0-linux-x64.tar.xz
 
-mv node*/bin/* /usr/local/bin/
+mv -f node*/bin/* /usr/local/bin/
 
 rm -rf /usr/include
 
-mv node*/include/node /usr/include
+mv -f node*/include/node /usr/include
 
 rm -rf /usr/lib/node_modules
 
-mv node*/lib/node_modules /usr/lib
+mv -f node*/lib/node_modules /usr/lib
